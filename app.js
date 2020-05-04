89a20d49ec78a989792b3c3e08cb11e83f45f5f6
delphinedara <delphine.dara@gmail.com>
2020-05-04 14:14:53 -0500

    modified app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const favicon = require('serve-favicon');
 require('./app_server/models/db');
 
 const indexRouter = require('./app_server/routes/index');
